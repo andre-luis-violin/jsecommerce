3b4c1500ae50b30ca8299441229d82425ddd5730
André Violin <andre.violin@novaandradina.org>
2021-12-17 23:40:39 -0400

    feat: add connection with DB

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,17 @@
 import express from 'express'
 import cors from 'cors'
-
+import mongoose from 'mongoose'
 import data from './data.js'
+import config from './config.js'
+
+mongoose
+  .connect(config.MONGODB_URL)
+  .then(() => {
+    console.log('Connected to mongodb')
+  })
+  .catch((error) => {
+    console.log(error.reason)
+  })
 
 const app = express()
 
@@ -9,7 +19,7 @@ app.use(cors())
 
 app.get('/api/products', (req, res) => {
   res.send(data.products)
-} )
+})
 
 app.listen(5000, () => {
   console.log('server at http://localhost:5000')
